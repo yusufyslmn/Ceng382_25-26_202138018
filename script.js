de97098cfacc27d9e59da22c91c9f0fe2f2f61d2
Yusuf YEŞİLMEN <yusufyesilmen@Yusuf-MacBook-Air.local>
2025-03-21 14:47:01 +0300

    redirection added

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-document.getElementById("loginBtn").addEventListener("click", function() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+document.addEventListener("DOMContentLoaded", function () {
+    let video = document.getElementById("backgroundVideo");
+    let button = document.getElementById("toggleButton");
 
-    if (username === "admin" && password === "admin") {
-        window.location.href = "table.html"; // Giriş başarılıysa yönlendirme
-    } else {
-        alert("Invalid username or password!"); // Hatalı giriş uyarısı
+    button.addEventListener("click", function () {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "h" || event.key === "H") {
+        let container = document.getElementById("registerContainer");
+        container.style.display = (container.style.display === "none") ? "block" : "none";
     }
+});
+
+document.getElementById("toggleButton").addEventListener("click", function() {
+    let container = document.getElementById("registerContainer");
+    container.style.display = (container.style.display === "none") ? "block" : "none";
+});
+
+function updateClock() {
+    let now = new Date();
+    let timeString = now.toLocaleTimeString();
+    document.getElementById("clock").textContent = timeString;
+}
+setInterval(updateClock, 1000);
+updateClock();
+
+let users = [];
+document.getElementById("loginBtn").addEventListener("click", function() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    users.push({ username, password });
+    console.log(users);
 });
